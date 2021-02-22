b0d3e0c422a5ed3aeb813b3399c7a282077d4512
Danish64 <danishali0051@gmail.com>
2021-02-23 01:28:46 +0500

    Postman Api Docgened

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const debug = require("debug")("app:db");
 const express = require("express");
 const app = express();
+const path = require("path");
 
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/prod")(app);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => debug(`Listening on port ${port}...`));
