69cb165e6bdbd0201584ab1854722d761470d72e
Danish64 <danishali0051@gmail.com>
2021-04-26 18:00:00 +0500

    socket initialized

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const debug = require("debug")("app:db");
+const path = require("path");
 const express = require("express");
 const app = express();
-const path = require("path");
+const httpServer = require("http").createServer(app);
+const options = {
+  cors: {
+    origin: "https://letusshare.herokuapp.com/",
+    methods: ["GET", "POST"],
+  },
+};
+const io = require("socket.io")(httpServer, options);
 
 require("./startup/routes")(app);
 require("./startup/db")();
@@ -10,5 +17,19 @@ require("./startup/prod")(app);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//Sockets
+
+const onConnection = (socket) => {
+  console.log("User Connected to letsshare server", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  //All event handlers
+  //eventHandler(io, socket);
+};
+
+io.on("connection", onConnection);
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
