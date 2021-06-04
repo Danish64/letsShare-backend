5922ac26c7c6af17fef24b0e07f2c4ebde932164
Danish64 <danishali0051@gmail.com>
2021-06-04 11:09:34 +0500

    Event Delete

--- controllers/Event/index.js
+++ controllers/Event/index.js
@@ -180,11 +180,16 @@ exports.createEventSharings = function (io) {
 exports.deleteShare = async (req, res) => {
   console.log("Delete Event route called");
   try {
-    Event.deleteOne({ _id: req.body.id }, function (err) {
+    Event.findByIdAndDelete(req.body.id, function (err) {
       if (err)
         return res
           .status(200)
           .send({ status: "Error", errorCode: 500, message: err.message });
+
+      return res.status(200).send({
+        status: "success",
+        message: `Event Deleted`,
+      });
     });
   } catch (err) {
     return res
